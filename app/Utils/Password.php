0ce12b0723bd0248abd926f23d5c50577dd18801
Ricardo García Jiménez <ricardogj08@riseup.net>
2024-01-25 19:00:32 -0600

    feat: Agrega clase de utilerías para contraseñas
    Signed-off-by: Ricardo García Jiménez <ricardogj08@riseup.net>

---
+++ app/Utils/Password.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Utils;
+
+class Password
+{
+    private static $algo = PASSWORD_DEFAULT;
+
+    /*
+     * Cifra una contraseña.
+     */
+    public static function encrypt(string $password)
+    {
+        return password_hash($password, self::$algo);
+    }
+
+    /*
+     * Comprueba que la contraseña coincida con el hash.
+     */
+    public static function verify(string $password, string $hash)
+    {
+        return password_verify($password, $hash);
+    }
+}
