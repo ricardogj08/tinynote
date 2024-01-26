995cd7c8889e215d48342823c6240390005a17bf
Ricardo García Jiménez <ricardogj08@riseup.net>
2024-01-26 03:08:40 -0600

    feat: Agrega middleware de autenticación
    Signed-off-by: Ricardo García Jiménez <ricardogj08@riseup.net>

---
+++ app/Middlewares/AuthMiddleware.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Middlewares;
+
+use App\Models\UserModel;
+use App\Utils\JWT;
+use PH7\JustHttp\StatusCode;
+use UnexpectedValueException;
+
+class AuthMiddleware
+{
+    /*
+     * Comprueba la autenticación de un token.
+     */
+    public function before($req, $res)
+    {
+        $header = $req->header('Authorization');
+
+        if (empty($header)) {
+            $res->status(StatusCode::UNAUTHORIZED)->json([
+                'errors' => 'Unauthorized endpoint',
+            ]);
+        }
+
+        /*
+         * Obtiene el token de autenticación
+         * desde el header de la petición.
+         */
+        $token = explode(' ', $header ?? '');
+        $token = end($token);
+
+        try {
+            // Decodifica el token de autenticación.
+            $payload = (new JWT())->decode($token);
+        } catch (UnexpectedValueException $e) {
+            $res->status(StatusCode::UNAUTHORIZED)->json([
+                'errors' => $e->getMessage()
+            ]);
+        }
+
+        // Consulta la información del usuario autenticado.
+        $userAuth = UserModel::factory()
+            ->select('id, active, is_admin')
+            ->find($payload->sub ?? null);
+
+        // Comprueba si el usuario está registrado.
+        if (empty($userAuth)) {
+            $res->status(StatusCode::UNAUTHORIZED)->json([
+                'errors' => 'Unauthorized user'
+            ]);
+        }
+
+        /*
+         * Se pasa la variable $req->app->local('userAuth')
+         * dentro de los middlewares y controladores
+         * con la información del usuario autenticado.
+         */
+        $req->app->local('userAuth', $userAuth);
+    }
+}
