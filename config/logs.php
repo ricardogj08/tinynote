931ad39a8a59dc8ab094e9801641ad8295ba73b4
Ricardo García Jiménez <ricardogj08@riseup.net>
2024-11-23 11:28:18 -0600

    feat: Optimiza opciones de configuración de los logs
    Signed-off-by: Ricardo García Jiménez <ricardogj08@riseup.net>

--- config/logs.php
+++ config/logs.php
@@ -1,24 +1,27 @@
 <?php
 
-use App\Utils\Env;
-
 /*
  * Opciones de configuración de los logs.
  */
 
 return (static function () {
-    $environment = Env::get('APP_ENVIRONMENT');
-
-    $is_dev = $environment === 'development';
-    $is_prod = $environment === 'production';
-
-    return [
-        'error_reporting' => ($is_dev ? E_ALL : (E_ALL & ~E_DEPRECATED & ~E_STRICT)),
-        'display_errors' => $is_dev,
-        'display_startup_errors' => $is_dev,
-        'log_errors' => $is_prod,
+    $options = [
+        'error_reporting' => (E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED),
+        'display_errors' => false,
+        'display_startup_errors' => false,
+        'log_errors' => true,
         'error_log' => '../writable/errors.log',
         'ignore_repeated_errors' => 1,
         'ignore_repeated_source' => 1
     ];
+
+    if (\App\Utils\Env::get('APP_ENVIRONMENT') === 'development') {
+        $options = array_merge($options, [
+            'error_reporting' => E_ALL,
+            'display_errors' => true,
+            'display_startup_errors' => true
+        ]);
+    }
+
+    return $options;
 })();
